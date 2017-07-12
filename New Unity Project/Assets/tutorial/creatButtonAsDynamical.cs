a7ca4a6e6d13b0923d8736c268cd572bd5efb554
las-devil <haho6629@gmail.com>
2017-07-12 10:50:57 +0900

    test, but error

---
+++ New Unity Project/Assets/tutorial/creatButtonAsDynamical.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creatButtonAsDynamical : MonoBehaviour {
+    GameObject buttonOR;
+	// Use this for initialization
+	void Start () {
+        buttonOR = GameObject.Find ("Canvas");
+        buttonOR.SetActive(false);
+	}
+    private void OnMouseDown()
+    {
+        GameObject button1 = Instantiate(buttonOR);
+        button1.transform.SetParent(buttonOR.transform);
+        button1.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
