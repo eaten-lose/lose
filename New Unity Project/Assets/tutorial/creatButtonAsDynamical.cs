1f7689bf69dac607780e0ffa59fedc7ff13e530e
las-devil <haho6629@gmail.com>
2017-07-12 15:25:54 +0900

    dynamic creat and read file

--- New Unity Project/Assets/tutorial/creatButtonAsDynamical.cs
+++ New Unity Project/Assets/tutorial/creatButtonAsDynamical.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class creatButtonAsDynamical : MonoBehaviour {
-    GameObject buttonOR;
+   public GameObject buttonOR;
+    public GameObject b;
 	// Use this for initialization
 	void Start () {
-        buttonOR = GameObject.Find ("Canvas");
-        buttonOR.SetActive(false);
-	}
+        string str;
+        FileStream read = new FileStream("da.txt", FileMode.Open);
+        StreamReader sR = new StreamReader(read);
+        str = sR.ReadLine();
+        Debug.Log(str);
+
+    }
     private void OnMouseDown()
     {
-        GameObject button1 = Instantiate(buttonOR);
-        button1.transform.SetParent(buttonOR.transform);
-        button1.SetActive(true);
+        buttonOR = GameObject.Find("B");
+        b=Instantiate(buttonOR);
+
+        b.transform.parent= GameObject.Find("Canvas").transform;
+        b.transform.Translate(new Vector3(200, 200, 30));
+
+        Debug.Log("las");
+        
     }
     // Update is called once per frame
     void Update () {
